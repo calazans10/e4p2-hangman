bba53f4c62dfc1dd9325971a6fbe6346411cdaeb
Jeferson Farias Calazans <calazans10@gmail.com>
2021-12-31 09:35:42 -0300

    Add a supervisor to dictionary

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,10 +1,12 @@
 defmodule Dictionary.Runtime.Server do
   @type t :: pid()
 
+  use Agent
+
   alias Dictionary.Impl.WordList
 
-  @spec start_link() :: {:ok, t}
-  def start_link do
+  @spec start_link(any) :: {:ok, t}
+  def start_link(_) do
     Agent.start_link(&WordList.word_list/0, name: __MODULE__)
   end
 
